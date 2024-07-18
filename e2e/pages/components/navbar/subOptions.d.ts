e5dc45d632b796fe79acf0afbadd6040969267d7
Vinicius Frasson <vinicius.smtp.cmd@gmail.com>
2024-07-17 22:25:01 -0300

    feat: add outher navbar itens

---
+++ e2e/pages/components/navbar/subOptions.d.ts
@@ -0,0 +1,43 @@
+export  enum SWITCHTO{
+    Alerts,
+    Windows,
+    Frames
+}
+
+export enum WIDGETS{
+    Accordion,
+    AutoComplete,
+    Datepicker,
+    Slider,
+}
+
+export enum INTERACTIONS{
+    DragandDrop="Drag and Drop",
+    Selectable,
+    Resizable
+
+
+}
+
+export enum VIDEO{
+    Youtube,
+    Vimeo
+}
+
+export enum WYSIWYG{
+    TinyMCE,
+    CKEditor,
+    SummerNote,
+    CodeMirror
+}
+
+export enum MORE{
+    Charts,
+    DynamicData="Dynamic Data",
+    FileDownload="File Download",
+    FileUpload="File Upload",
+    JQueryProgressBar="JQuery ProgressBar",
+    Loader,
+    Modals,
+    ProgressBar
+}

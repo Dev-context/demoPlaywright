0120c0b32ea5258fede6992118017511441f9800
Vinicius Frasson <vinicius.smtp.cmd@gmail.com>
2024-08-02 13:58:47 -0300

    feat: add github actions

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,10 +1,10 @@
-import { expect, test } from "fixture/baseTest";
+import { expect, test } from "fixture/baseTest"
 
 test.describe("Teste on Home page", () => {
-  test("Login com sucesso", async ({ home, page }) => {
-    await home.fillEmail("teste@test.com");
-    await home.performEnter();
+	test("Login com sucesso", async ({ home, page }) => {
+		await home.fillEmail("teste@test.com")
+		await home.performEnter()
 
-    expect(page.url()).toContain("Register.html");
-  });
-});
+		expect(page.url()).toContain("Register.html")
+	})
+})

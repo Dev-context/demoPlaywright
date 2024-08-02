15da7363ed9ec3ae48ebaae81465b77b7d684e74
Vinicius Frasson <vinicius.smtp.cmd@gmail.com>
2024-08-02 19:18:26 -0300

    feat: add anothers types os dialogs test elements

--- tests/e2e/alerts.spec.ts
+++ tests/e2e/alerts.spec.ts
@@ -1,27 +1,25 @@
-import { expect, test } from "fixture/baseTest";
+import { expect, test } from "fixture/baseTest"
 
 test.describe("Handle alerts Tests", () => {
-  test.beforeEach(async ({ home, navBar }) => {
-    await home.signUp("test@test.com");
-    await navBar.openOptionsSwitchTo("Alerts");
-  });
+	test.beforeEach(async ({ home, navBar }) => {
+		await home.signUp("test@test.com")
+		await navBar.openOptionsSwitchTo("Alerts")
+	})
 
-  test("Open Alerts with sucess", async ({ alerts }) => {
-    await alerts.alertWindowHandler();
-    await alerts.openAlertWithOk();
-  });
+	test("Open Alerts with sucess", async ({ alerts }) => {
+		await alerts.alertWindowHandler()
+		await alerts.openAlertWithOk()
+	})
 
-  test("Open confirmed Alert success", async ({ alerts }) => {
-    await alerts.openAlertWithOkCancel();
-    await alerts.alertCancelWindowHandler();
-    expect(alerts.alertConfirmText).toHaveText("You pressed Ok");
-  });
+	test("Open confirmed Alert success", async ({ alerts }) => {
+		await alerts.openAlertWithOkCancel()
+		await alerts.alertCancelWindowHandler()
+		expect(alerts.alertConfirmText).toHaveText("You pressed Ok")
+	})
 
-  test("Open prompt success", async ({ alerts }) => {
-    await alerts.openAlertWithTextbox();
-    await alerts.alertPromptlWindowHandler();
-    await expect(alerts.alertPromptText).toContainText(
-      "Testando com Playwright"
-    );
-  });
-});
+	test("Open prompt success", async ({ alerts }) => {
+		await alerts.openAlertWithTextbox()
+		await alerts.alertPromptlWindowHandler()
+		await expect(alerts.alertPromptText).toContainText("Testando com Playwright")
+	})
+})

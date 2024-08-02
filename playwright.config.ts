ef5578a7a490dcaa7f804006074fce592f9830e3
Vinicius Frasson <vinicius.smtp.cmd@gmail.com>
2024-08-02 14:05:16 -0300

    feat: add v4 upload artefact

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test"
 
 export default defineConfig({
-  testDir: "tests/e2e",
-  timeout: 30 * 1000,
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ["html"],
-    [
-      "allure-playwright",
-      {
-        outputFile: "my-allure-results",
-        includeProjectInTestName: true,
-        open: "always",
-      },
-    ],
-  ],
-  use: {
-    baseURL: "https://demo.automationtesting.in",
-    trace: "on",
-    screenshot: "on",
-    video: "on",
-  },
+	testDir: "tests/e2e",
+	timeout: 30 * 1000,
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: [
+		["html"],
+		[
+			"allure-playwright",
+			{
+				outputFile: "my-allure-results",
+				includeProjectInTestName: true,
+				open: "always",
+			},
+		],
+	],
+	use: {
+		baseURL: "https://demo.automationtesting.in",
+		trace: "on",
+		screenshot: "on",
+		video: "on",
+	},
 
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-  ],
-});
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
+})
